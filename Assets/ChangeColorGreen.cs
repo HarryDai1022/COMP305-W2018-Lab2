710b8a1b050bbc9a2e4f266aea234396f4839423
Harry Dai <dz655618@live.com>
2018-02-04 05:03:32 -0500

    Initial commit

---
+++ Assets/ChangeColorGreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorGreen : MonoBehaviour {
+    public SpriteRenderer character;
+    public Sprite Lab2Chara3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        character.sprite = Lab2Chara3;
+    }
+}
---
+++ Assets/ChangeColorRed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorRed : MonoBehaviour {
+    public SpriteRenderer character;
+    public Sprite Lab2Chara1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        character.sprite = Lab2Chara1;
+    }
+}
